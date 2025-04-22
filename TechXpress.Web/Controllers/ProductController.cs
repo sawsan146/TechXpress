d6f39267f4da83d421b9d01cf1be7c2cb0e250b0
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2025-04-23 01:36:55 +0200

    Add Connection string to appsetting

---
+++ TechXpress.Web/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechXpress.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View("AllProducts");
+        }
+    }
+}
