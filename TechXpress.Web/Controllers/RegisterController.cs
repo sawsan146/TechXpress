c00594e9d05472e7136e2f75a69b4105cccb6a45
NadeenElnasharty <nadenmagdy15@gmail.com>
2025-04-24 01:26:07 +0200

    add login

---
+++ TechXpress.Web/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechXpress.Web.Models;
+
+namespace TechXpress.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
