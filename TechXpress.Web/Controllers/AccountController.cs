84cdacb691fa3e0bda6d7014f3d993b21c93346f
NadeenElnasharty <nadenmagdy15@gmail.com>
2025-04-24 22:16:37 +0200

    Saving my changes before pull

---
+++ TechXpress.Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechXpress.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+    }
+}
