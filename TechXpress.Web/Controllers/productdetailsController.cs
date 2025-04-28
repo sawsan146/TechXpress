7b5d6cc91b95f3b5d7b179a41ef5be01537dc7ed
Aya Ahmed <aya.ahmed.swe@gmail.com>
2025-04-28 18:50:01 +0300

    product details page added, the search bar in the navbar modified

---
+++ TechXpress.Web/Controllers/productdetailsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechXpress.Web.Controllers
+{
+    public class productdetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
