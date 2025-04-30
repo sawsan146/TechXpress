b27466600f11a636bc0d3c15dda6a0c8f03d8105
Aya Ahmed <aya.ahmed.swe@gmail.com>
2025-05-01 02:14:43 +0300

    added wishlist page

---
+++ TechXpress.Web/ViewModel/WishListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechXpress.Web.ViewModel
+{
+    public class WishListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
