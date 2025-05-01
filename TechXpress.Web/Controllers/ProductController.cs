37486e8466c4d4d23a30a2f0db444b0cc062a000
NadeenElnasharty <nadenmagdy15@gmail.com>
2025-05-01 16:44:30 +0300

    edit category

--- TechXpress.Web/Controllers/ProductController.cs
+++ TechXpress.Web/Controllers/ProductController.cs
@@ -16,14 +16,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 1,
                     Category_ID = 1,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 400.00f,
                     Stock = 50,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 1, Name = "Phones" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 1, Product_ID = 1, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 1, Product_ID = 1, ImageURL = "~/Product/25e0a5d6-5154-45ce-a9ca-161096acc032.jpeg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -35,14 +34,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 2,
                     Category_ID = 2,
                     Name = "Product 2",
-                    Description = "Reliable product for everyday use.",
                     Price = 300.00f,
                     Stock = 30,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 2, Name = "Computers" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 2, Product_ID = 2, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 2, Product_ID = 2, ImageURL = "~/product/71Hk-ZbQAuL.__AC_SY300_SX300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -54,14 +52,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 3,
                     Category_ID = 3,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 540.00f,
                     Stock = 20,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 3, Name = "Smart Watches" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 3, Product_ID = 3, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 3, Product_ID = 3, ImageURL = "~/product/613Ay8pU9zL.__AC_SX300_SY300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -73,14 +70,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 4,
                     Category_ID = 4,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 900.00f,
                     Stock = 15,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 4, Name = "Cameras" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 4, Product_ID = 4, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 4, Product_ID = 4, ImageURL = "~/product/81RLTx5tIUL._AC_SL1500_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -92,14 +88,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 5,
                     Category_ID = 5,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 200.00f,
                     Stock = 40,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 5, Name = "Head Phones" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 5, Product_ID = 5, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 5, Product_ID = 5, ImageURL = "~/product/da6eac57-b91b-45aa-81f8-6185d4f60a6c.jpeg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -111,14 +106,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 6,
                     Category_ID = 6,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 340.00f,
                     Stock = 25,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 6, Name = "Games" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 6, Product_ID = 6, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 6, Product_ID = 6, ImageURL = "~/controller-6032446_640.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -130,14 +124,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 7,
                     Category_ID = 7,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 670.00f,
                     Stock = 60,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 7, Name = "Tablet" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 7, Product_ID = 7, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 7, Product_ID = 7, ImageURL = "~/product/5a666c35-7feb-4ac6-8e84-868d875094cd.jpeg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -149,14 +142,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 8,
                     Category_ID = 8,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 100.00f,
                     Stock = 150,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 8, Name = "Accessories" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 8, Product_ID = 8, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 8, Product_ID = 8, ImageURL = "~/product/ff07d5fd-2b23-4a90-890e-e2620eca3b21.jpeg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -168,14 +160,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 9,
                     Category_ID = 9,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 1200.00f,
                     Stock = 10,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 9, Name = "TV" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 9, Product_ID = 9, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 9, Product_ID = 9, ImageURL = "~/product/71VufAVwfIL.__AC_SY300_SX300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -187,14 +178,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 10,
                     Category_ID = 10,
                     Name = "Product 2",
-                    Description = "Reliable product for everyday use.",
                     Price = 800.00f,
                     Stock = 35,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 10, Name = "Laptops" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 10, Product_ID = 10, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 10, Product_ID = 10, ImageURL = "~/product/5fdcc66d-9d3a-40d8-b578-79570055618d.png" }
                     },
                     Reviews = new List<Review>
                     {
@@ -206,14 +196,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 11,
                     Category_ID = 11,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 250.00f,
                     Stock = 50,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 11, Name = "Printers" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 11, Product_ID = 11, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 11, Product_ID = 11, ImageURL = "~/product/51IIRfll9hL.__AC_SY300_SX300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -225,14 +214,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 12,
                     Category_ID = 12,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 150.00f,
                     Stock = 80,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 12, Name = "Monitors" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 12, Product_ID = 12, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 12, Product_ID = 12, ImageURL = "~/product/61Bo1IYATOL.__AC_SY300_SX300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -244,14 +232,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 13,
                     Category_ID = 13,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 180.00f,
                     Stock = 70,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 13, Name = "Speakers" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 13, Product_ID = 13, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 13, Product_ID = 13, ImageURL = "~/product/51-LfrxrhsL.__AC_SY300_SX300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -263,14 +250,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 14,
                     Category_ID = 1,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 450.00f,
                     Stock = 45,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 1, Name = "Phones" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 14, Product_ID = 14, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 14, Product_ID = 14, ImageURL = "~/product/61Ll0048EIL.__AC_SX300_SY300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -282,14 +268,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 15,
                     Category_ID = 2,
                     Name = "Product 2",
-                    Description = "Reliable product for everyday use.",
                     Price = 320.00f,
                     Stock = 28,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 2, Name = "Computers" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 15, Product_ID = 15, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 15, Product_ID = 15, ImageURL = "~/product/61rBqHWhl1L.__AC_SX300_SY300_QL70_ML2_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
@@ -301,14 +286,13 @@ namespace TechXpress.Web.Controllers
                     Product_ID = 16,
                     Category_ID = 3,
                     Name = "Product 1",
-                    Description = "High-quality product with advanced features.",
                     Price = 600.00f,
                     Stock = 18,
                     AddTime = DateTime.Now,
                     Category = new Category { Category_ID = 3, Name = "Smart Watches" },
                     ProductImages = new List<ProductImg>
                     {
-                        new ProductImg { Image_ID = 16, Product_ID = 16, ImageURL = "~/images/Products/Laptop.jpg" }
+                        new ProductImg { Image_ID = 16, Product_ID = 16, ImageURL = "~/product/61pa+yejx3L._AC_SX569_.jpg" }
                     },
                     Reviews = new List<Review>
                     {
