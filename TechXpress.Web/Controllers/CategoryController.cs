063747cddaab33acfa3d011c898b2fb747013377
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2025-05-02 11:50:56 +0300

    #

--- TechXpress.Web/Controllers/CategoryController.cs
+++ TechXpress.Web/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TechXpress.Web.ViewModel;
 
 namespace TechXpress.Web.Controllers
@@ -9,6 +10,21 @@ namespace TechXpress.Web.Controllers
         {
           
             return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult CategoryDashboard()
+        {
+            ViewData["ActivePage"] = "Category";
+            var categories = new List<CategoryViewModel>
+            {
+                new CategoryViewModel { Id = "1", Name = "Electronics", IsSelected = true },
+                new CategoryViewModel { Id = "2", Name = "Clothing", IsSelected = false },
+                new CategoryViewModel { Id = "3", Name = "Home Appliances", IsSelected = true },
+                new CategoryViewModel { Id = "4", Name = "Books", IsSelected = false },
+            };
+
+            return View(categories);
         }
     }
 }
